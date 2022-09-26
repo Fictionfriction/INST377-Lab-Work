afd8ccbc2c9d827ff2ce4f2a56b2ab346767617d
Jason Kearney <76897730+Fictionfriction@users.noreply.github.com>
2022-09-25 23:15:01 -0400

    move prev slide

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -49,6 +49,11 @@ function moveToNextSlide() {
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
+  if (slidePosition === 0) {
+    slidePosition = 3;
+  } else {
+    slidePosition -= 1;
+  }
   // add your code in here for when you click the "prev" button
   /*
     add an if statement here that checks
